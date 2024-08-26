5065c31b6a7876396ec0f9b521f20caf6f58008a
AyushBhardwaj <ayush7bhardwaj@gmail.com>
2024-08-26 17:14:48 +0530

    commit 4

--- src/JavaStreamsOneAgain/SortAccToLength.java
+++ src/JavaStreamsOneAgain/SortAccToLength.java
@@ -14,6 +14,6 @@ public class SortAccToLength {
                 return 1;
             return -1;
         }).forEach(System.out::println);
-        System.out.println("Test conflictingsg"+strings);
+        System.out.println("Test conflict"+strings);
     }
 }
